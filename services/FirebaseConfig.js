28ab1d5068f083414e8e3e5c86cc33bc9fce79af
arruttor <vtorg.martins@gmail.com>
2024-11-27 21:31:21 -0300

    adicionando config do banco

---
+++ services/FirebaseConfig.js
@@ -0,0 +1,12 @@
+
+const firebaseConfig = {
+  apiKey: "AIzaSyApx-e-otbE4fBdgyBqlIcMCG_UpDjmeDQ",
+  authDomain: "sistemadistribuidos-2024-2.firebaseapp.com",
+  projectId: "sistemadistribuidos-2024-2",
+  storageBucket: "sistemadistribuidos-2024-2.firebasestorage.app",
+  messagingSenderId: "261767267759",
+  appId: "1:261767267759:web:3d12b5a48cb17be2f39b5a"
+};
+
+
+  export default firebaseConfig;
